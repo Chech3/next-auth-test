eae7268aabd2597f7f345891322d3fd874330548
Chech3 <joseangelgarces123@gmail.com>
2024-11-29 11:42:46 -0400

    Avanzando en la prueba jeje

--- app/api/auth/register/route.ts
+++ app/api/auth/register/route.ts
@@ -1,10 +1,59 @@
-const { NextResponse } = require("next/server")
+import { NextResponse, NextRequest } from "next/server";
+import prisma from "@/libs/db";
+import bcrypt from "bcrypt";
 
-
-export async function POST(request) {
+export async function POST(request: NextRequest) {
+  try {
     const data = await request.json();
-    console.log(data);
+  console.log(data);
+  const emailFound = await prisma.user.findUnique({
+    where: { email: data.email },
+  });
+  const userFound = await prisma.user.findUnique({
+    where: { username: data.username },
+  });
 
-    return NextResponse.json("registering....")
+  if (emailFound) {
+    return NextResponse.json(
+      {
+        message: "email already exist",
+      },
+      {
+        status: 400,
+      }
+    );
+  }
+  if (userFound) {
+    return NextResponse.json(
+      {
+        message: "username already exist",
+      },
+      {
+        status: 400,
+      }
+    );
+  }
+
+// data.password = await bcrypt.hash(data.password, 10)
+const hashedPassword = await bcrypt.hash(data.password, 10)
+  const newUser = await prisma.user.create({
+    data: {
+        username: data.username,
+        email: data.email,
+        password: hashedPassword
+    }
+  });
+
+  const {password: _, ...user} = newUser //De esta forma ocultamos la contraseña para que no la devuelva en caso de requerir devoolver informacion
+
+  return NextResponse.json(user);
+  } catch (error ) {
+    const e = error as Error;
+    return NextResponse.json(
+    {
+        message: e.message,
+    },
+    {status: 500}
+)
+  }
 }
-
--- app/auth/register/page.tsx
+++ app/auth/register/page.tsx
@@ -1,12 +1,8 @@
 "use client";
 import { useState, FormEvent, ChangeEvent } from "react";
+import { FormData } from "@/app/interfaces";
+import { redirect } from "next/dist/server/api-utils";
 
-interface FormData {
-  username: string;
-  email: string;
-  password: string;
-  confirmPassword: string;
-}
 
 const RegisterPage = () => {
   const [data, setData] = useState<FormData>({
@@ -16,17 +12,28 @@ const RegisterPage = () => {
     confirmPassword: "",
   });
 
+  if (data.password !== data.confirmPassword){
+    console.log("laas conttraseñas no coinciden")
+  }
+
   const sendData = async (e: FormEvent) => {
     e.preventDefault();
     const res = await fetch("/api/auth/register", {
       method: 'POST',
-      body: JSON.stringify(data),
+      body: JSON.stringify({
+        username: data.username,
+        email: data.email,
+        password: data.password
+
+      }),
       headers: {
         'Content-Type' : 'application/son'
       }
     })
       const resJson = await res.json()
-      console.log(resJson);
+
+      if (res.ok) {
+        redirect('/'); 
   };
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
@@ -38,35 +45,44 @@ const RegisterPage = () => {
   };
 
   return (
-    <div className="flex items-center justify-center h-[calc(100vh-7rem)]">
+    <div className="flex flex-col items-center justify-center h-[calc(100vh-7rem)]">
+      <h1 className="text-3xl text-white font-bold">Formulario XD</h1>
       <form
         className="bg-gray-300 flex flex-col p-10 space-y-2 text-black"
         onSubmit={sendData}
         
       >
+        <label className="text-slate-600" htmlFor="username">Name</label>
         <input
+          id="username"
           name="username"
           value={data.username}
           onChange={handleChange}
           type="text"
         />
+         <label className="text-slate-600" htmlFor="email">email</label>
         <input
           value={data.email}
           onChange={handleChange}
           type="email"
           name="email"
+          id="email"
         />
+         <label className="text-slate-600" htmlFor="password">password</label>
         <input
           value={data.password}
           onChange={handleChange}
           type="password"
           name="password"
+          id="password"
         />
+        <label className="text-slate-600" htmlFor="confirmPassword">confirmPassword</label>
         <input
           value={data.confirmPassword}
           onChange={handleChange}
           type="password"
           name="confirmPassword"
+          id="confirmPassword"
         />
 
         <button className="px-2 py-2 rounded-md bg-gray-500" type="submit">
